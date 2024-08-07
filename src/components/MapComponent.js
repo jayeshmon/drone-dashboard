1595b2eecffb589249fe1421bf67fac7b86c7bc6
jayeshs.transight <jayeshs.transight@gmail.com>
2024-08-08 01:38:10 +0530

    state added

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -48,7 +48,7 @@ const MapComponent = ({ lat, lng, zoom, markers }) => {
 
       if (droneLocations.length > 0) {
         setCenter({ lat: lat || droneLocations[0].lat, lng: lng || droneLocations[0].lng });
-        setCurrentZoom(zoom || 14);
+        setCurrentZoom(zoom || 8);
       }
     };
 
